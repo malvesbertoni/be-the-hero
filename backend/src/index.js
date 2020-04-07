953870c3ca02c5cfe56c57b2005ecbb4d4a10adf
malvesbertoni <malvesbertoni@gmail.com>
2020-04-07 17:25:50 -0300

    App's back-end

---
+++ backend/src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * Métodos HTTP
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Tipos de parâmetros
+ * Query: Parâmetros nomeados enviados na rota após "?" (filtros, paginação)
+ *      http://localhost:3333/users?page=2&name=Mateus&idade=23
+ *      const params = request.query
+ * Route: Parâmetros utilizados para identificar recursos
+ *      http://localhost:3333/users/:id   |||   http://localhost:3333/users/1
+ *      const params = request.params
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ *      utiliza POST, envia em json
+ */
+
+
+app.listen(3333);
